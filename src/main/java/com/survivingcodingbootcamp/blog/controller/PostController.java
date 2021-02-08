31ab82c65f67b04460ebd7676fa9af0edeb69a41
Carl Dolvin <carl.dolvin@gmail.com>
2021-02-08 14:50:25 -0500

    fixed mapping in post controller

--- src/main/java/com/survivingcodingbootcamp/blog/controller/PostController.java
+++ src/main/java/com/survivingcodingbootcamp/blog/controller/PostController.java
@@ -1,29 +1,41 @@
 package com.survivingcodingbootcamp.blog.controller;
 
+import com.survivingcodingbootcamp.blog.model.Comment;
 import com.survivingcodingbootcamp.blog.model.Hashtag;
 import com.survivingcodingbootcamp.blog.model.Post;
 import com.survivingcodingbootcamp.blog.model.Topic;
-import com.survivingcodingbootcamp.blog.storage.HashtagStorage;
-import com.survivingcodingbootcamp.blog.storage.PostStorage;
-import com.survivingcodingbootcamp.blog.storage.TopicStorage;
+import com.survivingcodingbootcamp.blog.storage.*;
+import com.survivingcodingbootcamp.blog.storage.repository.commentRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Properties;
 
 @Controller
 @RequestMapping("/posts")
 public class PostController {
     private PostStorage postStorage;
+
     private TopicStorage topicStorage;
-    private HashtagStorage hashtagStorage;
+    private commentRepository commentRepo;
 
-    public PostController(PostStorage postStorage) {
+    public PostController(PostStorage postStorage, TopicStorage topicStorage,commentRepository commentRepo) {
         this.postStorage = postStorage;
-    }
+       this.topicStorage=topicStorage;
+       this.commentRepo=commentRepo;
 
-    @GetMapping("/{id}")
-    public String displaySinglePost(@PathVariable Long id, Model model) {
+    }
+@RequestMapping("/{id}")
+    public String displaySinglePost(@PathVariable long id, Model model) {
         model.addAttribute("post", postStorage.retrievePostById(id));
         return "single-post-template";
     }
-}
+@PostMapping("addPost/{id}")
+    public String addNewPost (@PathVariable long id,@RequestParam String newContent, @RequestParam String newTitle,@RequestParam String newAuthor){
+
+    Post addPost = new Post(newTitle,topicStorage.retrieveSingleTopic(id),newContent,newAuthor);
+      postStorage.save(addPost);
+      return "redirect:/topics/{id}";
+
+    }
